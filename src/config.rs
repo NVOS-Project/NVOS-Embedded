e81a7452d938adcddd2f14451cf5ed7452fe525b
LifetimeMistake <57274704+LifetimeMistake@users.noreply.github.com>
2023-08-06 04:11:29 +0200

    Implement configuration file Use JSON as config storage format Use values from config in GPIO borrow checker

---
+++ src/config.rs
@@ -0,0 +1,233 @@
+use std::collections::HashMap;
+use std::fmt::Display;
+use serde::{Serialize, Deserialize};
+use serde_json::Value;
+use std::io::{Read, Write};
+
+#[derive(Debug, PartialEq)]
+pub enum ConfigError {
+    SerializeError(String),
+    InvalidEntry(String),
+    MissingEntry(String),
+    Other(String)
+}
+
+impl Display for ConfigError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&match self {
+            ConfigError::SerializeError(msg) => format!("serialize/parse error: {}", msg),
+            ConfigError::InvalidEntry(msg) => format!("invalid config entry: {}", msg),
+            ConfigError::MissingEntry(msg) => format!("missing config entry: {}", msg),
+            ConfigError::Other(msg) => format!("config error: {}", msg)
+        })
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct ConfigSectionGPIO {
+    pub pin_config: HashMap<u8, u8>
+}
+
+impl ConfigSectionGPIO {
+    pub fn new(pin_config: HashMap<u8, u8>) -> Self {
+        Self { pin_config }
+    }
+
+    pub fn validate(&self) -> Result<(), ConfigError> {
+        let mut known_pin_ids = Vec::new();
+        let mut known_bcm_ids = Vec::new();
+
+        for (id, bcm) in &self.pin_config {
+            if known_pin_ids.contains(id) {
+                return Err(ConfigError::InvalidEntry(
+                    format!("invalid pin configuration: ({} -> {}), pin ID {} is defined more than once", id, bcm, bcm)
+                ));
+            }
+
+            if known_bcm_ids.contains(bcm) {
+                return Err(ConfigError::InvalidEntry(
+                    format!("invalid pin configuration: ({} -> {}), pin BCM ID {} is defined more than once", id, bcm, bcm)
+                ));
+            }
+
+            known_pin_ids.push(id.clone());
+            known_bcm_ids.push(bcm.clone());
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DeviceConfig {
+    pub driver: String,
+    pub data: HashMap<String, Value>
+}
+
+impl DeviceConfig {
+    pub fn new(driver: String, data: HashMap<String, Value>) -> Self {
+        Self { driver, data }
+    }
+
+    pub fn new_without_data(driver: String) -> Self {
+        Self { driver, data: HashMap::new() }
+    }
+
+    pub fn validate(&self) -> Result<(), ConfigError> {
+        if self.driver.trim().is_empty() {
+            return Err(ConfigError::InvalidEntry("invalid device config: driver name cannot be empty".to_string()));
+        }
+
+        for (key, _) in &self.data {
+            if key.trim().is_empty() {
+                return Err(ConfigError::InvalidEntry("invalid device data: key cannot be empty".to_string()));
+            }
+        } 
+
+        Ok(())
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct ConfigSectionDevices {
+    pub devices: Vec<DeviceConfig>
+}
+
+impl ConfigSectionDevices {
+    pub fn new(devices: Vec<DeviceConfig>) -> Self {
+        Self { devices }
+    }
+
+    pub fn validate(&self) -> Result<(), ConfigError> {
+        for device in &self.devices {
+            device.validate()?;
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct BusControllerConfig {
+    pub bus: String,
+    pub data: HashMap<String, Value>
+}
+
+impl BusControllerConfig {
+    pub fn new(bus: String, data: HashMap<String, Value>) -> Self {
+        Self { bus, data }
+    }
+
+    pub fn new_without_data(bus: String) -> Self {
+        Self { bus, data: HashMap::new() }
+    }
+
+    pub fn validate(&self) -> Result<(), ConfigError> {
+        if self.bus.trim().is_empty() {
+            return Err(ConfigError::InvalidEntry("invalid bus controller config: bus name cannot be empty".to_string()));
+        }
+
+        for (key, _) in &self.data {
+            if key.trim().is_empty() {
+                return Err(ConfigError::InvalidEntry("invalid bus controller data: key cannot be empty".to_string()));
+            }
+        } 
+
+        Ok(())
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct ConfigSectionControllers {
+    pub controllers: Vec<BusControllerConfig>   
+}
+
+impl ConfigSectionControllers {
+    pub fn new(controllers: Vec<BusControllerConfig>) -> Self {
+        Self { controllers }
+    }
+
+    pub fn validate(&self) -> Result<(), ConfigError> {
+        for bus in &self.controllers {
+            bus.validate()?;
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct Configuration {
+    pub gpio_section: ConfigSectionGPIO,
+    pub device_section: ConfigSectionDevices,
+    pub controller_section: ConfigSectionControllers
+}
+
+impl Configuration {
+    pub fn new(gpio_section: ConfigSectionGPIO, device_section: ConfigSectionDevices, controller_section: ConfigSectionControllers) -> Self {
+        Self { gpio_section, device_section, controller_section }
+    }
+
+    pub fn validate(&self) -> Result<(), ConfigError> {
+        self.gpio_section.validate()?;
+        self.device_section.validate()?;
+        self.controller_section.validate()?;
+        Ok(())
+    }
+}
+
+pub struct JsonConfigurationReader;
+impl JsonConfigurationReader {
+    pub fn from_reader<R: Read>(reader: R) -> Result<Configuration, ConfigError> {
+        let config: Configuration = match serde_json::from_reader(reader) {
+            Ok(c) => c,
+            Err(e) => {
+                return Err(ConfigError::SerializeError(
+                    format!("failed to deserialize config file: {}", e)
+                ));
+            }
+        };
+    
+        config.validate()?;
+        Ok(config)
+    }
+
+    pub fn from_str(json_str: String) -> Result<Configuration, ConfigError> {
+        Self::from_reader(json_str.as_bytes())        
+    }
+}
+
+pub struct JsonConfigurationWriter;
+impl JsonConfigurationWriter {
+    pub fn to_writer<W: Write>(config: &Configuration, writer: W, pretty: bool) -> Result<(), ConfigError> {
+        let result;
+        if pretty {
+            result = serde_json::to_writer_pretty(writer, config);
+        } else {
+            result = serde_json::to_writer(writer, config);
+        }
+        
+        match result {
+            Ok(_) => Ok(()),
+            Err(e) => Err(ConfigError::SerializeError(
+                format!("failed to serialize config: {}", e)
+            ))
+        }
+    }
+
+    pub fn to_str(config: &Configuration, pretty: bool) -> Result<String, ConfigError> {
+        let result;
+        if pretty {
+            result = serde_json::to_string_pretty(config);
+        } else {
+            result = serde_json::to_string(config);
+        }
+
+        match result {
+            Ok(s) => Ok(s),
+            Err(e) => Err(ConfigError::SerializeError(
+                format!("failed to serialize config: {}", e)
+            )),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,105 @@
-mod tests;
-mod device;
-mod capabilities;
 mod bus;
+mod capabilities;
+mod config;
+mod device;
 mod gpio;
 mod rpc;
+mod tests;
 
-use std::{error::Error, collections::HashMap, sync::Arc};
-use gpio::{GpioBorrowChecker, PinState};
+use config::{ConfigError, Configuration, JsonConfigurationReader, JsonConfigurationWriter};
 use device::DeviceServerBuilder;
+use gpio::{GpioBorrowChecker, PinState};
+use log::{error, info, warn, LevelFilter};
 use parking_lot::RwLock;
-use tonic::transport::Server;
 use rpc::reflection::{device_reflection_server::DeviceReflectionServer, DeviceReflectionService};
+use simple_logger::SimpleLogger;
+use std::{
+    collections::HashMap,
+    error::Error,
+    fs::File,
+    io::{BufReader, BufWriter},
+    path::Path,
+    sync::Arc,
+};
+use tonic::transport::Server;
 
 const SERVE_ADDR: &str = "0.0.0.0:30000";
-
-// TODO: implement loading from persistent storage
-fn load_pin_config() -> Result<HashMap<u8, PinState>, Box<dyn Error>> {
-    let mut pins = HashMap::new();
-    for i in 1..=40 {
-        let pin = PinState::new(i, i+20);
-        pins.insert(i, pin);
-    }
-
-    Ok(pins)
-}
+const CONFIG_PATH: &str = "nvos_config.json";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    println!("Building GPIO borrow checker");
-    let pins = load_pin_config()?;
-    let gpio_borrow = Arc::new(RwLock::new(GpioBorrowChecker::new(pins)));
+    SimpleLogger::new()
+        .with_colors(true)
+        .with_level(LevelFilter::Info)
+        .init()?;
 
+    info!("Loading configuration file at {}", CONFIG_PATH);
+    let config;
+
+    if !Path::new(CONFIG_PATH).exists() {
+        warn!("Config file does not exist or is inaccessible");
+        warn!("Creating default config file");
+        config = Configuration::default();
+
+        match File::create(CONFIG_PATH) {
+            Ok(f) => {
+                let writer = BufWriter::new(f);
+                match JsonConfigurationWriter::to_writer(&config, writer, true) {
+                    Ok(_) => info!("Config file written to {}", CONFIG_PATH),
+                    Err(e) => error!("Failed to write config file: {}", e),
+                };
+            }
+            Err(e) => error!("Failed to open config file for write: {}", e),
+        }
+    } else {
+        config = match File::open(CONFIG_PATH)
+            .map_err(|err| ConfigError::Other(format!("failed to read config file: {}", err)))
+            .and_then(|f| JsonConfigurationReader::from_reader(BufReader::new(f)))
+        {
+            Ok(c) => c,
+            Err(e) => {
+                error!(
+                    "Failed to read config file at location {}: {}",
+                    CONFIG_PATH, e
+                );
+                warn!("Using default config file instead.");
+                Configuration::default()
+            }
+        };
+    }
+
+    info!("Building GPIO borrow checker");
+    if config.gpio_section.pin_config.len() == 0 {
+        warn!("Config does not have any GPIO entires. This will not work.");
+    }
+    
+    let gpio_borrow = Arc::new(RwLock::new(GpioBorrowChecker::new(
+        config
+            .gpio_section
+            .pin_config
+            .iter()
+            .map(|(pin_id, bcm_id)| {
+                (
+                    pin_id.clone(),
+                    PinState::new(pin_id.clone(), bcm_id.clone()),
+                )
+            })
+            .collect(),
+    )));
+    // TODO: build device server from config
     println!("Building device server");
-    let device_server = Arc::new(RwLock::new(DeviceServerBuilder::configure()
-        .build()
-        .expect("failed to build device server")));
+    let device_server = Arc::new(RwLock::new(
+        DeviceServerBuilder::configure()
+            .build()
+            .expect("failed to build device server"),
+    ));
 
     // Serve gRPC
     let rpc_server = Server::builder()
         .tcp_nodelay(true)
-        .add_service(DeviceReflectionServer::new(DeviceReflectionService::new(&device_server)))
+        .add_service(DeviceReflectionServer::new(DeviceReflectionService::new(
+            &device_server,
+        )))
         .serve(String::from(SERVE_ADDR).parse().unwrap());
 
     println!("Server running on {}!", SERVE_ADDR);
